57b468e0ab78a9a2915ff42626eb7629d1cc1d30
Bambobello <bellobambo21@gmail.com>
2025-12-08 23:40:31 +0100

    adjust gas fee

--- src/hooks/useCollegeFundraiser.ts
+++ src/hooks/useCollegeFundraiser.ts
@@ -211,7 +211,7 @@ export function useCollegeFundraiser() {
       functionName: "fundTicket",
       args: [ticketId],
       value: parseEther(amount),
-      gas: BigInt(200_000),
+      gas: BigInt(400_000),
     });
   };
 
